b5be2ec7bb3c3be6a03025c1c8336a241dcb4ae7
1DontEx1st <github@idontexist.com>
2017-10-19 23:51:00 -0400

    Update Logging.js

--- scripts/Logging.js
+++ scripts/Logging.js
@@ -18,13 +18,30 @@ function AddLogCards( aSection, aReward ) {
 /**
 * Updated Gui with Arena or Adventure energy.
 */
-function updateEnergy( aCheck, aMax, aSection ) {
+function updateEnergy( energyCurrent, energyMax, energySection ) {
     var mySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName( "Settings" );
-    if ( aSection == 'Arena' ) {
-        mySheet.getRange( "D6" ).setValue( 'Arena Energy: ' + aCheck + '/' + aMax );
+    if ( energySection == 'Arena' ) {
+        mySheet.getRange( "D6" ).setValue( 'Arena Energy: ' + energyCurrent + '/' + energyMax );
     } else {
-        mySheet.getRange( "C6" ).setValue( 'Adventure Energy: ' + aCheck + '/' + aMax );
+        mySheet.getRange( "C6" ).setValue( 'Adventure Energy: ' + energyCurrent + '/' + energyMax );
     }
+}
+
+/**
+* Update Gui with Swole status.
+*/
+function updateSwole( hero, time ) {
+    var mySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName( "Settings" );
+    mySheet.getRange( "C7" ).setValue( 'Training ' + idToHero[hero] + ', ' + (time/60/60).toFixed(2) + ' hours.' );
+}
+
+/**
+* Update Gui when Swole is disabled.
+*/
+function disableSwole() {
+    var mySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName( "Settings" );
+    mySheet.getRange( "C7" ).setValue( 'Disabled' ).setBackground( 'black' );
+//    mySheet.getRange( "C7" ).setBackground( 'black' );
 }
 
 /**
@@ -33,9 +50,9 @@ function updateEnergy( aCheck, aMax, aSection ) {
 function updateNext( aCheck ) {
     var mySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName( "Settings" );
     if ( aCheck == true ) {
-        mySheet.getRange( "C7" ).setValue( 'Next check ' + myFormattedTimeNext() );
+        mySheet.getRange( "C8" ).setValue( 'Next check ' + myFormattedTimeNext() );
     } else {
-        mySheet.getRange( "C7" ).setValue( 'Disabled at ' + formattedTime() );
+        mySheet.getRange( "C8" ).setValue( 'Disabled at ' + formattedTime() );
     }
 }
 
@@ -45,9 +62,9 @@ function updateNext( aCheck ) {
 function updateNextRumble( aCheck,aTime ) {
     var mySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName( "Settings" );
     if ( aCheck == true ) {
-        mySheet.getRange( "C8" ).setValue( 'Next Rumble Check ' + aTime );
+        mySheet.getRange( "C9" ).setValue( 'Next Rumble Check ' + aTime );
     } else {
-        mySheet.getRange( "C8" ).setValue( 'Disabled at ' + formattedTime() );
+        mySheet.getRange( "C9" ).setValue( 'Disabled at ' + formattedTime() );
     }
 }
 
@@ -71,12 +88,26 @@ function addLog( aSection, aReward ) {
 function writeLogs( aSection, aRow ) {
     var myEmptyRow = getFirstEmptyRow();
     var myCount = 0
-    var myString = ''
-    var myString = getProperty( aSection );
+    var myString = getProperty( aSection )+"";
     myCount = getProperty( aSection + '_count' );
     var mySheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName( 'Logs' );
     if ( myString != null ) {
         mySheet.getRange( aRow + "" + myEmptyRow ).setNote( myString );
+        if (aSection == "BuyCardAndRecycle") {
+            if (myString.indexOf("Rarity:4") > 0) {
+              mySheet.getRange( aRow + "" + myEmptyRow ).setBackground('#b517dc');
+              mySheet.getRange( aRow + "" + myEmptyRow ).setFontColor('white');
+            } else if (myString.indexOf("Rarity:3") > 0) {
+              mySheet.getRange( aRow + "" + myEmptyRow ).setBackground('#26f2ff');
+              mySheet.getRange( aRow + "" + myEmptyRow ).setFontColor('white');
+            } else if (myString.indexOf("Rarity:2") > 0) {
+              mySheet.getRange( aRow + "" + myEmptyRow ).setBackground('#87d97a');
+              mySheet.getRange( aRow + "" + myEmptyRow ).setFontColor('white');
+            } else {
+              mySheet.getRange( aRow + "" + myEmptyRow ).setBackground('#efe3e3');
+              //mySheet.getRange( aRow + "" + myEmptyRow ).setFontColor('white');
+            }
+        }
     }
     mySheet.getRange( aRow + "" + myEmptyRow ).setValue( myCount );
     setProperty( aSection, '' )
@@ -143,73 +174,86 @@ function WriteLogs() {
 * return error/rewards string
 */
 function parseRewards( aRewards ) {
-    var ItemInfo = UrlFetchApp.fetch( getProperty( '_url' ) + '&message=useItem' );
-    var ItemInfo_json = JSON.parse( ItemInfo );
-    var rewards_Json = JSON.parse( aRewards )[ 0 ]
-    if ( rewards_Json.gold != null ) {
-        var Gold = rewards_Json.gold
+    var itemInfo = UrlFetchApp.fetch( getProperty( '_url' ) + '&message=useItem' );
+    var itemInfoJson = JSON.parse( itemInfo );
+    var rewardsJson = JSON.parse( aRewards )[ 0 ];
+    Logger.log(aRewards);
+    Logger.log(rewardsJson);
+    if ( rewardsJson.gold != null ) {
+        var gold = rewardsJson.gold;
     } else {
-        var Gold = 0
+        var gold = 0;
     }
-    if ( rewards_Json.sp != null ) {
-        var Sp = rewards_Json.sp
+    Logger.log("rewardsJson.hero_xp = " + rewardsJson.hero_xp);
+    if ( rewardsJson.hero_xp != null ) {
+        Logger.log("found tokens");
+        var tokens = "" + idToHero[parseInt(rewardsJson.hero_xp.hero_id)] + ": " + rewardsJson.hero_xp.xp;
     } else {
-        var Sp = 0
+        Logger.log("no tokens found");
+        var tokens = 0;
     }
-    if ( rewards_Json.xp != null ) {
-        var xp = rewards_Json.xp
+    if ( rewardsJson.sp != null ) {
+        var sp = rewardsJson.sp;
     } else {
-        var xp = 0
+        var sp = 0;
     }
-    if ( rewards_Json.rating_change != null ) {
-        var RatingChange = rewards_Json.rating_change
+    if ( rewardsJson.xp != null ) {
+        var xp = rewardsJson.xp;
     } else {
-        var RatingChange = 0
+        var xp = 0;
     }
-    if ( rewards_Json.guild_war_points != null ) {
-        var GuildWarPoints = rewards_Json.guild_war_points
+    if ( rewardsJson.rating_change != null ) {
+        var ratingChange = rewardsJson.rating_change;
     } else {
-        var GuildWarPoints = 0
+        var ratingChange = 0;
     }
-    var items = ''
-    var string = ''
-    if ( rewards_Json.item != null ) {
-        var itemlist = rewards_Json.item
-    } else if ( rewards_Json.items != null ) {
-        var itemlist = rewards_Json.items
+    if ( rewardsJson.guild_war_points != null ) {
+        var guildWarPoints = rewardsJson.guild_war_points;
+    } else {
+        var guildWarPoints = 0;
+    }
+    var items = '';
+    var string = '';
+    if ( rewardsJson.item != null ) {
+        var itemlist = rewardsJson.item;
+    } else if ( rewardsJson.items != null ) {
+        var itemlist = rewardsJson.items;
     }
     if ( itemlist != null ) {
         for ( var i = 0; i < itemlist.length; i++ ) {
             var item = itemlist[ i ].id;
-            var Number = itemlist[ i ].number;
+            var number = itemlist[ i ].number;
             if ( item == 30001 || item == 30002 ) {
-                var item_name = 'Adcrate'
+                var itemName = 'Adcrate';
             } else { //They are removed from the inventory before it can id them.
-                var item_name = ItemInfo_json.user_items[ item ].name;
+                var itemName = itemInfoJson.user_items[ item ].name;
             }
-            items = items + ' [' + item_name + ':' + Number + ']'
+            items = items + ' [' + itemName + ':' + number + ']';
         }
     }
-    if ( Gold != 0 ) {
-        string = string + ' Gold:' + Gold
+    if ( gold != 0 ) {
+        string = string + ' Gold:' + gold;
     }
-    if ( Sp != 0 ) {
-        string = string + ' Wats:' + Sp
+    if ( sp != 0 ) {
+        string = string + ' Watts:' + sp;
     }
-    if ( GuildWarPoints != 0 ) {
-        string = string + ' Guild War Points:' + GuildWarPoints
+    if ( guildWarPoints != 0 ) {
+        string = string + ' Guild War Points:' + guildWarPoints;
     }
     if ( xp != 0 ) {
-        string = string + ' xp:' + xp
+        string = string + ' xp:' + xp;
     }
-    if ( RatingChange != 0 ) {
-        string = string + ' RatingChange:' + RatingChange
+    if ( ratingChange != 0 ) {
+        string = string + ' RatingChange:' + ratingChange;
     }
     if ( items != '' ) {
-        string = string + ' items:' + items
+        string = string + ' items:' + items;
+    }
+    if ( tokens != '' ) {
+        string = string + ' tokens:' + tokens;
     }
     if ( string == '' ) {
         string = 'No rewards/Failed to load rewards'
     }
-    return string
+    return string;
 }
